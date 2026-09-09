c87eb7e3f2414db8c0ab7891ae65b60e82dc7a7c
Sean Deniel Dayrit <seandeniel3@gmail.com>
2026-09-09 21:14:57 +0800

    Hi

--- src/Users/User.java
+++ src/Users/User.java
@@ -1,5 +1,31 @@
 package Users;
 
 public class User {
+
+
+    protected String userID;
+    protected String userType;
+
+    public User(String userID, String userType) {
+        this.userID = userID;
+        this.userType = userType;
+    }
     
+    public String getUserId() {
+        return userID;
+    }
+
+    public void setUserId(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public abstract void navigate();
+
+
+    public abstract boolean aunthenticate(); 
+>>>>>>> 21af9d9 (Hi)
 }
