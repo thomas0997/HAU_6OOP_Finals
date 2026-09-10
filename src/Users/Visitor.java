dca0abe0687d57289dce4f793c110e4e91cc441a
Sean Deniel Dayrit <seandeniel3@gmail.com>
2026-09-10 16:38:09 +0800

    Finish Visitor.java

--- src/Users/Visitor.java
+++ src/Users/Visitor.java
@@ -1,8 +1,23 @@
 package Users;
 
 
-public class Visitor {
+public class Visitor extends User {
+    private String purposeOfVisit, typeOfID;
 
+    public Visitor (String userid, String usertype, String purposeOfVisit, String typeOfID) {
+        super(userid, usertype);
+        this.purposeOfVisit = purposeOfVisit;
+        this.typeOfID = typeOfID;
+    }
+
+    @Override 
+    public void navigate() {
+        System.out.println("Visitor access: public locations onl");
+    }
+
+    public boolean authenticate(){
+        return true;
+    }
 
 
     
