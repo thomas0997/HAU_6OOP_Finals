5a5873eeb259705ef8588e2dae8045038b6889a3
Sean Deniel Dayrit <seandeniel3@gmail.com>
2026-09-10 16:15:03 +0800

    Finish User.java

--- src/Users/User.java
+++ src/Users/User.java
@@ -1,5 +1,15 @@
 package Users;
 
-public class User {
+public abstract class User {
+    private String userid;
+    private String usertype;
 
+    public User(String userid, String usertype) {
+        this.userid = userid;
+        this.usertype = usertype;
+    }
+
+    public abstract void navigate();
+
+    public abstract boolean authenticate();
 }
