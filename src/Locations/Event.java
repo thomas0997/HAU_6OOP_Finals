0cdd27035a18dedb48a9a456115d83088818c088
Francis <nithanbiag07@gmail.com>
2026-09-13 01:00:48 +0800

    Implement Event class extending CampusEntity with encapsulation and getInfo override

--- src/Locations/Event.java
+++ src/Locations/Event.java
@@ -1,5 +1,33 @@
 package Locations;
 
-public class Event {
-    
+public class Event extends CampusEntity {
+    private String dateTime;
+    private String organizer;
+
+    public Event(String id, String name, String description, double x, double y, String dateTime, String organizer) {
+        super(id, name, description, x, y);
+        this.dateTime = dateTime;
+        this.organizer = organizer;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    @Override
+    public String getInfo() {
+        return "[Event] " + getName() + " | " + dateTime + " | Organizer: " + organizer;
+    }
 }
