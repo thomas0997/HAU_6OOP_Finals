50e809941f386d9bce125bc46e0bfdf404741c84
thomas0997 <thomasfranco9907@gmail.com>
2026-09-14 11:39:32 +0800

    Debug Event.java

--- src/Locations/Event.java
+++ src/Locations/Event.java
@@ -4,8 +4,10 @@ public class Event extends CampusEntity {
     private String dateTime;
     private String organizer;
 
-    public Event(String id, String name, String description, double x, double y, String dateTime, String organizer) {
-        super(id, name, description, x, y);
+
+
+    public Event(String name, String description, double x, double y, String id, String dateTime, String organizer) {
+        super(name, description, x, y, id);
         this.dateTime = dateTime;
         this.organizer = organizer;
     }
