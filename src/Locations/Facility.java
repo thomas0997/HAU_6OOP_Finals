351f0b11b139c9f7eab51f7379b094bcd130bc52
Francis <nithanbiag07@gmail.com>
2026-09-14 12:04:06 +0800

    Implement Facility class extending CampusEntity with encapsulation and getInfo override

--- src/Locations/Facility.java
+++ src/Locations/Facility.java
@@ -1,5 +1,23 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from IntelliJ IDEA).
+
 package Locations;
 
-public class Facility {
+public class Facility extends CampusEntity {
+ 
+    private String facilityType;
+ 
+    public Facility(String id, String name, String description, double x, double y,
+                     String facilityType) {
+        super(id, name, description, x, y);
+        this.facilityType = facilityType;
+    }
+ 
+    public String getFacilityType() {
+        return facilityType;
+    }
     
+    @Override
+    public String getInfo() {
+        return "[Facility] " + getName() + " │ Type: " + facilityType;
+    }
 }
