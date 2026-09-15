ffa07981a48a5e7b11fd043f9620505514d334ae
Danielrein-hub <deinrein018@gmail.com>
2026-09-15 09:56:28 +0800

    Finalize Commit of Location Files

--- src/Locations/AcademicBuilding.java
+++ src/Locations/AcademicBuilding.java
@@ -1,5 +1,15 @@
 package Locations;
 
-public class AcademicBuilding {
-    
+public class AcademicBuilding extends Building {
+    private String[] coursesOffered;
+
+    public AcademicBuilding(String name, String description, double x, double y, String id, int floorCount, String[] facilities, String[] coursesOffered) {
+        super(name, description, x, y, id, floorCount, facilities);
+        this.coursesOffered = coursesOffered;
+    }
+
+    @Override
+    public String getInfo() {
+        return "[Academic Building] " + getName() + " | Courses: " + String.join(", ", coursesOffered);
+    }
 }
--- src/Locations/Building.java
+++ src/Locations/Building.java
@@ -1,5 +1,17 @@
 package Locations;
 
-public class Building {
-    
+public class Building extends CampusEntity {
+    private int floorCount;
+    private String[] facilities;
+
+    public Building(String name, String description, double x, double y, String id, int floorCount, String[] facilities) {
+        super(name, description, x, y, id);
+        this.floorCount = floorCount;
+        this.facilities = facilities;
+    }
+
+    @Override
+    public String getInfo() {
+        return "[Building] " + getName() + " | Floors: " + floorCount;
+    }
 }
